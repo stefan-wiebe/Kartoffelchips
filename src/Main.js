08d61c90d9709533c059b7d15974d892f92b605b
Tobias Timpe <tobias@tim.pe>
2015-09-01 14:01:49 +0200

    loading mirror

--- src/Main.js
+++ src/Main.js
@@ -27,6 +27,8 @@ function initGame() {
 	loadSprite('prism');
 	loadSprite('receiver');
   loadSprite('inventory');
+  loadSprite('mirror');
+  
   var mHandler = new Mouse();
 	requestAnimationFrame(tick);
 
