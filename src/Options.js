4af14853779d2a3f6f56c61b6da858aa38672e92
Tobias Timpe <tobias@tim.pe>
2015-09-03 12:53:00 +0200

    options.js

---
+++ src/Options.js
@@ -0,0 +1,14 @@
+// Options.js
+options = {
+	"debug": false,
+	"showFPS": true
+};
+
+if (localStorage.getItem('options') != null) {
+	Util.log('Loading options from localStorage');
+	options = JSON.parse(localStorage.getItem('options'));
+}
+function saveOptions() {
+	Util.log('Saving options to localStorage');
+	localStorage.setItem('options', JSON.stringify(options));
+}
