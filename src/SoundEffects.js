e3f3e4792257a7e50a20d2b1188cfa7b985a8f1f
Tobias Timpe <tobias@tim.pe>
2015-09-04 22:51:57 +0200

    Added basic sound effects module

--- src/SoundEffects.js
+++ src/SoundEffects.js
@@ -3,8 +3,18 @@ var SoundEffects = function () {
 	this.ac = new AudioContext();
 
 };
-SoundEffects.startLaserSoundEffect = function() {
+SoundEffects.prototype.startLaserSoundEffect = function() {
+	var filter = this.ac.createBiquadFilter();
+	filter.type = 'bandpass';
+	filter.Q.value = 0.75390625;
+	filter.frequency.value = 350;
 	var osc = this.ac.createOscillator();
 	osc.type = 'sawtooth';
-	
+	osc.frequency.value = 95;
+	osc.connect(filter);
+	filter.connect(this.ac.destination);
+	osc.start();
+
+
+
 };
