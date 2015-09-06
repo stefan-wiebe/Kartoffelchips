9eaed56f117c70ff4b78fe2069ebfd37299afba0
StW96 <3istee@abwesend.de>
2015-09-06 23:28:37 +0200

    Fixed merge failures.

--- src/Main.js
+++ src/Main.js
@@ -63,8 +63,6 @@ var credits = [{
 function initGame() {
     c = document.getElementById('game');
     if (checkBrowserCompatibility) {
-        document.body.style.background = 'none #000';
-        document.getElementById('warning').style.display = 'none';
         for (var i = 0; i < 16; i++) {
             map[i] = [];
 
